c242c8d01b570b9b0f5b27c0e4610d6cc940d99c
Mohamed\ Sharaf <mohamedsharf26797@gmail.com>
2021-08-01 20:38:29 +0200

    repository files restructure

--- Back/dbConnection/mongo_connect.js
+++ Back/dbConnection/mongo_connect.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-const atlasURI = 'mongodb+srv://dbAdmin:dbAdminPass@FoudCourtTask.konjn.mongodb.net/FoudCourtTask?retryWrites=true&w=majority'
+const atlasURI = 'mongodb+srv://dbAdmin:dbAdminPass@food-court.konjn.mongodb.net/food-court?retryWrites=true&w=majority'
 mongoose.connect(atlasURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => console.log("connected to Database"))
     .catch(err => console.log("error connecting to database : ", err));
